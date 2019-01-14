b331cbc19cc7293af31bf19d3967598de84f6411
jbg <jiben_gong@outlook.com>
2019-01-14 22:24:38 +0800

    classmate-list

---
+++ list-classmates.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+classmate = ['dylan','calara','doctor']
+classmate.append('marx')
+classmate.insert(1, 'nexus')
+
+
+print(classmate)
+
+print(len(classmate))
+
+#print(classmate[0])
+#print(classmate[1])
+#print(classmate[2])
+#print(classmate[3])
+
+print(classmate[-1])
+print(classmate[-2])
+print(classmate[-3])
+print(classmate[-4])
+print(classmate[-5])
+
